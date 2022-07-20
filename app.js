be9b00b004542f0684517d02c2e8d9b880f70c85
danbaziewicz <danxbazi@gmail.com>
2022-07-20 05:27:52 -0300

    feat: adicionado app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-import promptSync from 'prompt-sync';
 import express from 'express';
-import dicaController from './src/controller/dica-controller.js';
-import dicas from './dicas-db.js'
+import dicaController from './src/controllers/dica-controller.js';
+import dicasArr from './src/infra/dicas-database.js';
 
-const app = express();
-const port = 3333;
+const app = express()
+const port = 3333
 
-const promp = promptSync();
-let entry = promp('Insira uma dica: ');
+app.use(express.json())
 
-do {
-    dicasArr.push(entry)
-    entry = prompt('Insira uma dica ou digite sair: ')
-    console.log(dicasArr)
-} while (entry.toLowerCase() !== 'sair')
 
-//chamar controler
 
+
+// do {
+//     dicasArr.push(entry)
+//     entry = prompt('Insira uma dica ou digite sair: ')
+//     console.log(dicasArr)
+// } while (entry.toLowerCase() !== 'sair')
+
+// TODO: Chamar os controllers 
 dicaController(app)
 
-app.listen(port, () => {
+app.listen(port, ()=> {
     console.log(`http://localhost:${port}/`)
 })
